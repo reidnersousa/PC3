2c924988ea1205bbde01e8706dd2c4af0fb36ad9
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2021-12-17 18:28:30 -0300

    Update CarroRepository.java

--- CarroRepository.java
+++ CarroRepository.java
@@ -1,5 +1,7 @@
 package br.com.prog3.semanapratica2.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import br.com.prog3.semanapratica2.domain.Carro;
 @Repository
 public interface CarroRepository extends CrudRepository<Carro, String>{
 
+	public Optional<Carro> findByPlaca(Integer placa);
+	
+	public void deleteByPlaca(Integer placa);
 }
+
