021404a7cc94df434ed237035de51979eae9e0b1
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2021-12-17 18:46:40 -0300

    Update CarroService.java

--- CarroService.java
+++ CarroService.java
@@ -1,19 +1,39 @@
 package br.com.prog3.semanapratica2.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.prog3.semanapratica2.domain.Carro;
-import br.com.prog3.semanapratica2.repository.CarroRepository;
+import br.com.prog3.semanapratica2.domain.Cliente;
+import br.com.prog3.semanapratica2.repository.ClienteRepository;
 
 @Service
-public class CarroService {
-	
+public class ClienteService {
+
 	
 	@Autowired
-	private CarroRepository carroRepository;
+	private ClienteRepository clienteRepository;
+	
+	public Cliente save(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteRepository.findAll();
+	}
 
-	public Carro save(Carro carro) {
-		return carroRepository.save(carro);
-		}
+	public Optional<Cliente> findByCpf(String cpf) {
+		return clienteRepository.findByCpf(cpf);
+	}
+
+	public Cliente update(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+
+	public void deleteByCpf(String cpf) {
+		clienteRepository.deleteByCpf(cpf);
+	}
+	
 }
